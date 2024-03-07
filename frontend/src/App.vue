39365ff03560d3ed38f9bad84632b5b9a73fb563
olzhasar <o.arystanov@gmail.com>
2024-03-08 00:15:22 +0500

    Configure vue.js with Vite and Tailwind CSS

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const ws = new WebSocket("ws://localhost:8080/ws");
+ws.onopen = () => {
+  console.log("connected");
+};
+ws.onmessage = (event) => {
+  console.log(event.data);
+};
+ws.onclose = () => {
+  console.log("disconnected");
+};
+
+let message = "";
+
+const sendMessage = () => {
+  ws.send(message);
+  message = "";
+};
+</script>
+
+<template>
+  <div class="text-center">
+    <h1 class="my-4 text-2xl">Chat</h1>
+
+    <input
+      v-model="message"
+      @keyup.enter="sendMessage"
+      class="p-2 w-1/2 border-2 border-gray-300"
+      type="text"
+      placeholder="Type a message"
+    />
+  </div>
+</template>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-

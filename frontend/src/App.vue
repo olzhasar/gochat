24f31fb136ececbf571f973a1675bc070e846013
olzhasar <o.arystanov@gmail.com>
2024-03-09 18:25:03 +0500

    Add send button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -74,7 +74,7 @@ const setName = () => {
 	</div>
 
 	<div v-else class="chat chat-end">
-	  <div class="chat-bubble chat-bubble-secondary">{{ msg.content }}</div>
+	  <div class="chat-bubble">{{ msg.content }}</div>
 	</div>
 
       </div>
@@ -85,7 +85,7 @@ const setName = () => {
       sendMessage();
     }
       "
-      class="my-4"
+      class="flex gap-2 pr-0 my-4"
       id="messageForm"
       v-show="nameSet"
     >
@@ -97,6 +97,12 @@ const setName = () => {
 	type="text"
 	placeholder="Type a message"
       />
+
+      <button class="btn btn-square btn-secondary" type="submit">
+	<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+</svg>
+      </button>
     </form>
 
   </div>

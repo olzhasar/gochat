f17b87ffef733b2cee84fbd6114bbdafc9f7dea4
olzhasar <o.arystanov@gmail.com>
2024-03-10 22:48:30 +0500

    Minor changes to the frontend

--- frontend/src/pages/Chat.vue
+++ frontend/src/pages/Chat.vue
@@ -159,14 +159,72 @@ const notifyStopTyping = () => {
     sendToWS("", MessageType.STOP_TYPING);
   }
 };
+
+const share = async () => {
+  if ("share" in navigator) {
+    try {
+      await navigator.share({
+        title: "Join my super secret chat room",
+        text: "Join my super secret chat room",
+        url: window.location.href,
+      });
+    } catch (error) {
+      console.error("Error sharing:", error);
+    }
+  } else {
+    alert(
+      "Sharing not supported on this browser. Please, copy the link manually.",
+    );
+  }
+};
 </script>
 
 <template>
   <main
     v-if="connected"
-    class="flex overflow-hidden flex-col p-2 mx-auto max-w-md h-screen md:p-0"
+    class="flex overflow-hidden flex-col px-2 pb-2 mx-auto max-w-md h-screen md:p-0"
   >
-    <h1 class="my-4 text-2xl text-center">Chat</h1>
+    <div class="flex navbar flex-0">
+      <div class="flex-0">
+        <a href="/" class="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
+          </svg>
+        </a>
+      </div>
+      <div class="flex-grow text-center">
+        <h1 class="w-full text-lg font-bold text-center">Super secret chat</h1>
+      </div>
+      <div class="flex-none">
+        <button class="btn btn-square btn-ghost" @click="share">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
 
     <form
       v-if="!nameSet"
@@ -192,7 +250,7 @@ const notifyStopTyping = () => {
     <div
       v-show="nameSet"
       id="messageList"
-      class="overflow-y-scroll flex-grow pr-2 my-4 space-y-2 no-scrollbar"
+      class="overflow-y-scroll flex-grow pr-2 space-y-2 no-scrollbar"
     >
       <div v-for="msg in messages">
         <div

5f630754b68fac864a3c25d3240d0f5f4446e862
olzhasar <o.arystanov@gmail.com>
2024-03-10 22:18:47 +0500

    Fix the bug in the chat page

--- frontend/src/pages/Chat.vue
+++ frontend/src/pages/Chat.vue
@@ -162,120 +162,119 @@ const notifyStopTyping = () => {
 </script>
 
 <template>
-  <div
+  <main
+    v-if="connected"
     class="flex overflow-hidden flex-col p-2 mx-auto max-w-md h-screen md:p-0"
   >
-    <main v-if="connected">
-      <h1 class="my-4 text-2xl text-center">Chat</h1>
+    <h1 class="my-4 text-2xl text-center">Chat</h1>
 
-      <form
-        v-if="!nameSet"
-        class="space-y-2"
-        @submit="
-          (event) => {
-            event.preventDefault();
-            setName();
-          }
-        "
-      >
-        <input
-          tabindex="0"
-          v-model="name"
-          class="w-full input input-bordered"
-          type="text"
-          placeholder="Enter your name"
-          autofocus
-        />
-        <button class="btn btn-primary btn-block">Start chatting</button>
-      </form>
+    <form
+      v-if="!nameSet"
+      class="space-y-2"
+      @submit="
+        (event) => {
+          event.preventDefault();
+          setName();
+        }
+      "
+    >
+      <input
+        tabindex="0"
+        v-model="name"
+        class="w-full input input-bordered"
+        type="text"
+        placeholder="Enter your name"
+        autofocus
+      />
+      <button class="btn btn-primary btn-block">Start chatting</button>
+    </form>
 
-      <div
-        v-show="nameSet"
-        id="messageList"
-        class="overflow-y-scroll flex-grow pr-2 my-4 space-y-2 no-scrollbar"
-      >
-        <div v-for="msg in messages">
-          <div
-            v-if="msg.msgType == MessageType.TEXT && msg.author != null"
-            class="chat chat-start"
-          >
-            <div class="chat-header">{{ msg.author }}</div>
-            <div class="chat-bubble chat-bubble-primary">
-              {{ msg.content }}
-            </div>
-          </div>
-
-          <div
-            v-if="msg.msgType == MessageType.TEXT && msg.author === null"
-            class="chat chat-end"
-          >
-            <div class="chat-bubble">{{ msg.content }}</div>
-          </div>
-
-          <p v-if="msg.msgType == MessageType.NAME" class="my-2 text-center">
-            {{ msg.author }} has joined the chat
-          </p>
-
-          <p v-if="msg.msgType == MessageType.LEAVE" class="my-2 text-center">
-            {{ msg.author }} has disconnected
-          </p>
-        </div>
-
-        <div v-if="personTyping" class="chat chat-start">
-          <div class="opacity-60 chat-footer">
-            {{ personTyping }} is typing...
+    <div
+      v-show="nameSet"
+      id="messageList"
+      class="overflow-y-scroll flex-grow pr-2 my-4 space-y-2 no-scrollbar"
+    >
+      <div v-for="msg in messages">
+        <div
+          v-if="msg.msgType == MessageType.TEXT && msg.author != null"
+          class="chat chat-start"
+        >
+          <div class="chat-header">{{ msg.author }}</div>
+          <div class="chat-bubble chat-bubble-primary">
+            {{ msg.content }}
           </div>
         </div>
+
+        <div
+          v-if="msg.msgType == MessageType.TEXT && msg.author === null"
+          class="chat chat-end"
+        >
+          <div class="chat-bubble">{{ msg.content }}</div>
+        </div>
+
+        <p v-if="msg.msgType == MessageType.NAME" class="my-2 text-center">
+          {{ msg.author }} has joined the chat
+        </p>
+
+        <p v-if="msg.msgType == MessageType.LEAVE" class="my-2 text-center">
+          {{ msg.author }} has disconnected
+        </p>
       </div>
 
-      <form
-        @submit="
-          (event) => {
-            event.preventDefault();
-            sendMessage();
-          }
-        "
-        class="flex gap-2 pr-0 my-4"
-        id="messageForm"
-        v-show="nameSet"
-      >
-        <input
-          tabindex="1"
-          id="messageInput"
-          v-model="messagePrompt"
-          class="w-full input input-bordered"
-          type="text"
-          placeholder="Type a message"
-          autocomplete="off"
-          @focus="scrollToBottom"
-          @input="notifyTyping"
-          @blur="notifyStopTyping"
-        />
-
-        <button class="btn btn-square btn-secondary" type="submit">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
-            />
-          </svg>
-        </button>
-      </form>
-    </main>
-
-    <div v-else class="flex justify-center items-center h-full">
-      <div class="flex flex-col items-center">
-        <h1 class="text-2xl">Connecting...</h1>
-        <span class="loading loading-spinner loading-lg"></span>
+      <div v-if="personTyping" class="chat chat-start">
+        <div class="opacity-60 chat-footer">
+          {{ personTyping }} is typing...
+        </div>
       </div>
+    </div>
+
+    <form
+      @submit="
+        (event) => {
+          event.preventDefault();
+          sendMessage();
+        }
+      "
+      class="flex gap-2 pr-0 my-4"
+      id="messageForm"
+      v-show="nameSet"
+    >
+      <input
+        tabindex="1"
+        id="messageInput"
+        v-model="messagePrompt"
+        class="w-full input input-bordered"
+        type="text"
+        placeholder="Type a message"
+        autocomplete="off"
+        @focus="scrollToBottom"
+        @input="notifyTyping"
+        @blur="notifyStopTyping"
+      />
+
+      <button class="btn btn-square btn-secondary" type="submit">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+          />
+        </svg>
+      </button>
+    </form>
+  </main>
+
+  <div v-else class="flex justify-center items-center h-full">
+    <div class="flex flex-col items-center">
+      <h1 class="text-2xl">Connecting...</h1>
+      <span class="loading loading-spinner loading-lg"></span>
     </div>
   </div>
 </template>

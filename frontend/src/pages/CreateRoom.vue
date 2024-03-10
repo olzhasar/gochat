74415701bfeebdd610be8cba73fa39308676dc97
olzhasar <o.arystanov@gmail.com>
2024-03-10 23:53:09 +0500

    Add loader for creating room

--- frontend/src/pages/CreateRoom.vue
+++ frontend/src/pages/CreateRoom.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import { useRouter } from "vue-router";
+import { ref } from "vue";
 
 const router = useRouter();
+
+let loading = ref(false);
 
 const createRoom = () => {
   const apiUrl = import.meta.env.VITE_API_URL as string;
   const url = apiUrl + "/room";
+
+  loading.value = true;
 
   fetch(url, {
     method: "POST",
@@ -20,11 +25,17 @@ const createRoom = () => {
 
 <template>
   <div class="mx-auto max-w-xs">
-    <h1 class="my-8 text-2xl text-center">Welcome to the chat!</h1>
-    <form @submit.prevent="createRoom" class="space-y-2">
-      <button type="submit" class="btn btn-primary btn-block">
-        Create new room
-      </button>
-    </form>
+    <main v-if="!loading">
+      <h1 class="my-8 text-2xl text-center">Welcome to the chat!</h1>
+      <form @submit.prevent="createRoom" class="space-y-2">
+        <button type="submit" class="btn btn-primary btn-block">
+          Create new room
+        </button>
+      </form>
+    </main>
+
+    <div v-else class="flex justify-center items-center h-screen">
+      <span class="loading loading-spinner loading-lg"></span>
+    </div>
   </div>
 </template>

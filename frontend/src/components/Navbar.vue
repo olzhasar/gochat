b9d01ac46ee85dd83ce8febbfd914586d1042b44
olzhasar <o.arystanov@gmail.com>
2024-03-11 13:31:11 +0500

    Add confirmation modal to home button in navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -16,12 +16,33 @@ const share = async () => {
     );
   }
 };
+
+const showModal = () => {
+  const modal = document.getElementById("modal-home") as HTMLDialogElement;
+  modal.showModal();
+};
 </script>
 
 <template>
+  <dialog id="modal-home" class="modal">
+    <div class="modal-box">
+      <h3 class="text-lg font-bold">Are you sure want to leave this room?</h3>
+      <div class="modal-action">
+        <form method="dialog">
+          <!-- if there is a button in form, it will close the modal -->
+          <a class="mr-4 btn btn-outline btn-primary" href="/">Yes</a>
+          <button class="btn">No</button>
+        </form>
+      </div>
+    </div>
+    <form method="dialog" class="modal-backdrop">
+      <button>close</button>
+    </form>
+  </dialog>
+
   <div class="flex navbar flex-0">
     <div class="flex-0">
-      <a href="/" class="btn btn-square btn-ghost">
+      <button class="btn btn-square btn-ghost" @click="showModal">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -36,7 +57,7 @@ const share = async () => {
             d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
           />
         </svg>
-      </a>
+      </button>
     </div>
     <div class="flex-grow text-center">
       <h1 class="w-full text-lg font-bold text-center">Super secret chat</h1>

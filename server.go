de430f4d751dd1bdb3d0eeacdb1311fea77e507d
olzhasar <o.arystanov@gmail.com>
2024-03-11 19:34:01 +0500

    Refactor server handlers. Get cors origin from env

--- server.go
+++ server.go
@@ -3,73 +3,87 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/websocket"
 )
 
 type Server struct {
-	mux      *http.ServeMux
-	upgrader websocket.Upgrader
-	hub      *Hub
+	mux             *http.ServeMux
+	upgrader        websocket.Upgrader
+	hub             *Hub
+	corsAllowOrigin string
 }
 
-func handleRoomCreate(hub *Hub) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		room := hub.CreateRoom()
+func (s *Server) handleRoomCreate(w http.ResponseWriter, r *http.Request) {
+	room := s.hub.CreateRoom()
 
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Content-Type", "text/plain")
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(room.ID))
-	}
+	s.setCORSPolicy(w)
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(room.ID))
 }
 
-func handleRoomGet(hub *Hub) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+func (s *Server) handleRoomGet(w http.ResponseWriter, r *http.Request) {
+	s.setCORSPolicy(w)
 
-		roomID := r.PathValue("room")
-		if roomID == "" {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		room := hub.GetRoom(roomID)
-		if room == nil {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-
-		w.WriteHeader(http.StatusNoContent)
+	roomID := r.PathValue("room")
+	if roomID == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
+
+	room := s.hub.GetRoom(roomID)
+	if room == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
 
-func handleWS(upgrader websocket.Upgrader, hub *Hub) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		roomID := r.PathValue("room")
-		if roomID == "" {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		room := hub.GetRoom(roomID)
-		if room == nil {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Println("Failed to upgrade connection")
-			log.Println(err)
-			return
-		}
-
-		client := NewClient(conn)
-		hub.Register(client, room)
-
-		hub.ListenClient(client, room)
+func (s *Server) handleWS(w http.ResponseWriter, r *http.Request) {
+	roomID := r.PathValue("room")
+	if roomID == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
+
+	room := s.hub.GetRoom(roomID)
+	if room == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	conn, err := s.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Failed to upgrade connection")
+		log.Println(err)
+		return
+	}
+
+	client := NewClient(conn)
+	s.hub.Register(client, room)
+
+	s.hub.ListenClient(client, room)
+}
+
+func (s *Server) setCORSPolicy(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", s.corsAllowOrigin)
+}
+
+func (s *Server) handleOptions(w http.ResponseWriter, r *http.Request) {
+	s.setCORSPolicy(w)
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (s *Server) configureRoutes() {
+	s.mux.HandleFunc("OPTIONS /*", s.handleOptions)
+	s.mux.HandleFunc("POST /room", s.handleRoomCreate)
+	s.mux.HandleFunc("GET /room/{room}", s.handleRoomGet)
+	s.mux.HandleFunc("GET /ws/{room}", s.handleWS)
 }
 
 func NewServer(hub *Hub) *Server {
@@ -79,17 +93,16 @@ func NewServer(hub *Hub) *Server {
 		CheckOrigin:     func(r *http.Request) bool { return true },
 	}
 	mux := http.NewServeMux()
-	mux.HandleFunc("OPTIONS /*", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		w.WriteHeader(http.StatusNoContent)
-	})
-	mux.HandleFunc("POST /room", handleRoomCreate(hub))
-	mux.HandleFunc("GET /room/{room}", handleRoomGet(hub))
-	mux.HandleFunc("GET /ws/{room}", handleWS(upgrader, hub))
 
-	return &Server{upgrader: upgrader, hub: hub, mux: mux}
+	var corsAllowOrigin string
+	if corsAllowOrigin = os.Getenv("CORS_ORIGIN"); corsAllowOrigin == "" {
+		corsAllowOrigin = "*"
+	}
+
+	server := &Server{upgrader: upgrader, hub: hub, corsAllowOrigin: corsAllowOrigin, mux: mux}
+	server.configureRoutes()
+
+	return server
 }
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
